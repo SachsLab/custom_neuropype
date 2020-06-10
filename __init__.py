4b988268b21b45b48ff493af76630b6ddc7e7949
Chadwick Boulay <chadwick.boulay@gmail.com>
2020-06-10 17:39:48 -0400

    Fix attempted import of removed node.

--- __init__.py
+++ __init__.py
@@ -3,6 +3,5 @@ from .NSLRHMM import NSLRHMM
 from .PupilToAngle import PupilToAngle
 from .UpdateElectrodePositions import UpdateElectrodePositions
 from .VariantLDA import VariantLDA
-from .MountainSort import MountainSort
 from .AsType import AsType
 from .ReconContFromEvents import ReconContFromEvents
